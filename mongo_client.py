74f4916e563073a6d9cb323fe7c9e25dd2add06e
swetasri13 <127306664+swetasri13@users.noreply.github.com>
2023-03-14 23:09:08 +0530

    Add files via upload
    added 500 SP stock names to pull data from YFinanace

--- mongo_client.py
+++ mongo_client.py
@@ -16,22 +16,33 @@ from pymongo import MongoClient
 
 yfin.pdr_override()
 
-stockList=[]
-stockList= ['GOOGL']
+# insert S&P500 constituent data
+sp_500_df= pd.read_csv('C:\sweta\portfolio-recommendation-engine\SP500 Companies.csv')
+
+
+stockList1=sp_500_df["Symbol"].astype(str).tolist()
+#print(stockList1)
+stockList= ['GOOGL','APPL','ACE', 'ABT', 'ANF', 'ACN', 'ADBE', 'AMD', 'AES', 'AET', 'AFL', 'A', 'GAS', 'APD', 'ARG', 'AKAM', 'AA', 'ALXN']
 
 enddate=dt.datetime.now()
 startdte=enddate-dt.timedelta(days=365)
-stockData = pdr.get_data_yahoo(stockList, startdte, enddate)
+stockData = pdr.get_data_yahoo(stockList1, startdte, enddate)
 
 client =  MongoClient("mongodb+srv://mongouser:market101@cluster0.fgigdzs.mongodb.net/?retryWrites=true&w=majority")
 db = client['journaldev']
 collection = db['stock_returns']
 stockData.reset_index(inplace=True)
 data_dict = stockData.to_dict("records")
-# Insert collection
+# Insert collection EOD prices
 collection.insert_many(data_dict)
 
-   
+#inset SP 500 constituents data
+sp500_collection = db["sp500_data"]
+sp_500_df.reset_index(inplace=True)
+sp_data_dict = sp_500_df.to_dict("records") 
+
+# Insert SP500 data collection
+sp500_collection.insert_many(sp_data_dict)
 
 
 # In[ ]:
